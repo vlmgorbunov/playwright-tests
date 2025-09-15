60bc4f22d87e2e0c833df9be1637014d3e2d4a6a
vlmgorbunov <vlm.gorbunov@gmail.com>
2025-09-15 04:49:27 +0300

    remove 1 test

--- tests/test-1.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  await expect(
-    page.getByRole('link', { name: 'Playwright logo Playwright' }),
-  ).toBeVisible();
-  await page.getByRole('link', { name: 'Agents' }).click();
-});
